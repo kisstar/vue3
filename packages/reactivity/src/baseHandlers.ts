affc723452d40eebbc5187481751079c0432d39b
Kisstar <dwh.chn@foxmail.com>
2025-11-14 21:30:11 +0800

    feat: 数组的响应式

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -17,15 +17,30 @@ export const mutableHandlers = {
   },
   set(target, key, value, receiver) {
     const oldValue = target[key]
-    const result = Reflect.set(target, key, value, receiver)
 
     if (isRef(oldValue) && !isRef(value)) {
       oldValue.value = value
-      return result
+      return true
     }
 
+    const targetIsArray = Array.isArray(target)
+    const len = targetIsArray ? target.length : 0
+    const result = Reflect.set(target, key, value, receiver)
+
     if (hasChanged(value, oldValue)) {
-      trigger(target, key, value)
+      trigger(target, key)
+    }
+
+    const newLen = targetIsArray ? target.length : 0
+
+    if (len !== newLen && key !== 'length') {
+      /**
+       * 隐式更新length
+       * 更新前：state = ['a', 'b', 'c', 'd']
+       * 更新后：state = ['a', 'b', 'c', 'd', 'e']
+       * 隐式更新原理：数组的 push、pop、shift、unshift、splice 方法会隐式更新 length 属性
+       */
+      trigger(target, 'length')
     }
 
     return result
--- packages/reactivity/src/dep.ts
+++ packages/reactivity/src/dep.ts
@@ -25,15 +25,42 @@ export function track(target, key) {
   link(dep, activeSub)
 }
 
-export function trigger(target, key, value) {
+export function trigger(target, key) {
   const depsMap = targetMap.get(target)
+  const targetIsArray = Array.isArray(target)
 
-  if (!depsMap) return
+  if (targetIsArray && key === 'length') {
+    /**
+     * 原始数组 ['a', 'b', 'c', 'd']
+     * 修改前： length = 4，数组为 ['a', 'b', 'c', 'd']
+     * 修改后： length = 2, 数组变为 ['a', 'b']
+     * 说明： 修改数组长度时，数组长度小于当前数组长度时，会删除从下标为数组长度到原数组长度的元素
+     * 结论： 修改数组长度时，需要通知访问了通过改变 length 删除的元素的副作用函数重新执行
+     *
+     * depsMap = {
+     *     0: Dep
+     *     1: Dep
+     *     2: Dep
+     *     3: Dep
+     *     length: Dep
+     * }
+     */
 
-  const dep = depsMap.get(key)
-  if (!dep) return
+    const len = target.length
 
-  propagate(dep.subs)
+    depsMap.forEach((dep, depKey) => {
+      if (Number(depKey) >= len || depKey === 'length') {
+        propagate(dep.subs)
+      }
+    })
+  } else {
+    if (!depsMap) return
+
+    const dep = depsMap.get(key)
+    if (!dep) return
+
+    propagate(dep.subs)
+  }
 }
 
 class Dep {
